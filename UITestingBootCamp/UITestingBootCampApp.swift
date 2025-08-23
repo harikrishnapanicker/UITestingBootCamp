f31a56b505b440f6f5736062def9f9a72755c356
Harikrishnapanicker <harikrishnapanicker@gmail.com>
2025-08-23 12:19:09 +0530

    Initial Commit

---
+++ UITestingBootCamp/UITestingBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingBootCampApp.swift
+//  UITestingBootCamp
+//
+//  Created by HariKrishna Panicker on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct UITestingBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UITestingBootCampTests/UITestingBootCampTests.swift
@@ -0,0 +1,16 @@
+//
+//  UITestingBootCampTests.swift
+//  UITestingBootCampTests
+//
+//  Created by HariKrishna Panicker on 23/08/25.
+//
+
+import Testing
+
+struct UITestingBootCampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
